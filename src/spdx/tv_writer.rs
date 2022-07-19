96459031f5c545a5666b2b850f98e47eec957a72
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-07-20 05:58:55 +0900

    write tag value for file information

--- src/spdx/tv_writer.rs
+++ src/spdx/tv_writer.rs
@@ -5,9 +5,9 @@ use std::fmt::Display;
 use chrono::SecondsFormat;
 use spdx_rs::models::{
     Algorithm, Annotation, Checksum, CreationInfo, DocumentCreationInformation,
-    ExternalDocumentReference, ExternalPackageReference, FileInformation,
-    OtherLicensingInformationDetected, PackageInformation, PackageVerificationCode, Relationship,
-    Snippet, SPDX,
+    ExternalDocumentReference, ExternalPackageReference, ExternalPackageReferenceCategory,
+    FileInformation, FileType, OtherLicensingInformationDetected, PackageInformation,
+    PackageVerificationCode, Relationship, Snippet, SPDX,
 };
 
 use super::WriteTagValue;
@@ -161,9 +161,12 @@ impl<W: std::io::Write> WriteTagValue<W> for PackageInformation {
         if let Some(package_verification_code) = &self.package_verification_code {
             package_verification_code.write_tag_value(write)?;
         }
-        for sum in &self.package_checksum {
-            write_tag_value_normal(write, "PackageChecksum", &format_checksum(sum))?;
-        }
+        write_tag_value_vec(
+            write,
+            "PackageChecksum",
+            &self.package_checksum.iter().map(format_checksum).collect(),
+        )?;
+
         write_tag_value_opt(write, "PackageHomePage", &self.package_home_page)?;
         write_tag_value_opt(
             write,
@@ -217,19 +220,84 @@ impl<W: std::io::Write> WriteTagValue<W> for PackageInformation {
 
 impl<W: std::io::Write> WriteTagValue<W> for PackageVerificationCode {
     fn write_tag_value(&self, write: &mut W) -> Result<(), std::io::Error> {
-        todo!()
+        let value = if self.excludes.len() == 0 {
+            format!("{}", self.value)
+        } else {
+            format!("{} (excludes: {})", self.value, self.excludes.join(" "))
+        };
+        write_tag_value_normal(write, "PackageVerificationCode", &value)?;
+        Ok(())
     }
 }
 
 impl<W: std::io::Write> WriteTagValue<W> for ExternalPackageReference {
     fn write_tag_value(&self, write: &mut W) -> Result<(), std::io::Error> {
-        todo!()
+        let category = match self.reference_category {
+            ExternalPackageReferenceCategory::Security => "SECURITY",
+            ExternalPackageReferenceCategory::PackageManager => "PACKAGE-MANAGER",
+            ExternalPackageReferenceCategory::PersistentID => "PERSISTENT-ID",
+            ExternalPackageReferenceCategory::Other => "OTHER",
+        };
+
+        write_tag_value_normal(
+            write,
+            "ExternalRef",
+            &format!(
+                "{} {} {}",
+                category, self.reference_type, self.reference_locator
+            ),
+        )?;
+        write_tag_value_opt(write, "ExternalComment", &self.reference_comment)?;
+        Ok(())
+    }
+}
+
+fn file_type_name(file_type: &FileType) -> &'static str {
+    match file_type {
+        FileType::Source => "SOURCE",
+        FileType::Binary => "BINARY",
+        FileType::Archive => "ARCHIVE",
+        FileType::Application => "APPLICATION",
+        FileType::Audio => "AUDIO",
+        FileType::Image => "IMAGE",
+        FileType::Text => "TEXT",
+        FileType::Video => "VIDEO",
+        FileType::Documentation => "DOCUMENTATION",
+        FileType::SPDX => "SPDX",
+        FileType::Other => "OTHER",
     }
 }
 
 impl<W: std::io::Write> WriteTagValue<W> for FileInformation {
+    /// https://spdx.github.io/spdx-spec/file-information/
     fn write_tag_value(&self, write: &mut W) -> Result<(), std::io::Error> {
-        todo!()
+        write_tag_value_normal(write, "FileName", &self.file_name)?;
+        write_tag_value_normal(write, "SPDXID", &self.file_spdx_identifier)?;
+        write_tag_value_vec(
+            write,
+            "FileType",
+            &self.file_type.iter().map(file_type_name).collect(),
+        )?;
+        write_tag_value_vec(
+            write,
+            "FileChecksum",
+            &self.file_checksum.iter().map(format_checksum).collect(),
+        )?;
+        write_tag_value_normal(write, "LicenseConcluded", &self.concluded_license)?;
+        write_tag_value_vec(
+            write,
+            "LicenseInfoInFile",
+            &self.license_information_in_file,
+        )?;
+        write_tag_value_opt(write, "LicenseComments", &self.comments_on_license)?;
+        write_tag_value_normal(write, "FileCopyrightText", &self.copyright_text)?;
+        write_tag_value_opt(write, "FileComment", &wrap_text_opt(&self.file_comment))?;
+        write_tag_value_opt(write, "FileNotice", &wrap_text_opt(&self.file_notice))?;
+        write_tag_value_vec(write, "FileContributor", &self.file_contributor)?;
+        if let Some(attribution) = &self.file_attribution_text {
+            write_tag_value_vec_text(write, "FileAttributionText", &attribution)?;
+        }
+        Ok(())
     }
 }
 
